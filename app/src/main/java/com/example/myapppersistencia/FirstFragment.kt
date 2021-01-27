9a4424861e17a1b10f17da77c37587d967bd077c
DANIELA VERA <d.vera29@gmail.com>
2021-01-26 20:13:14 -0600

    ejercicio terminado se añaden boton de guardar y borrar. se ingresa y se guarda un Int, String y Float.

--- app/src/main/java/com/example/myapppersistencia/FirstFragment.kt
+++ app/src/main/java/com/example/myapppersistencia/FirstFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.example.myapppersistencia.databinding.ActivityMainBinding
@@ -32,48 +33,56 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentFirstBinding.inflate(inflater, container, false)
         myapppersistencia =
-            this.context?.getSharedPreferences(mFileSharedPreferences, Context.MODE_PRIVATE)
-                ?: return binding.root
+            requireActivity().getSharedPreferences(mFileSharedPreferences, Context.MODE_PRIVATE)
+
+        return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.guardarEntero.text = myapppersistencia.getInt("Clave1", 0).toString()
+        binding.guardarTexto.text = myapppersistencia.getString("Danny", "").toString()
+        binding.guardarDecimal.text = myapppersistencia.getFloat("number", 0.0f).toString()
+        var decimal = "number"
+        var claveEntero = "Clave1"
+        var textoingresado = "Danny"
+        binding.buttonGuardar.setOnClickListener {
+
+            var valor = binding.editNumber.text.toString().toInt()
+            myapppersistencia.edit().putInt(claveEntero, valor).apply()
+            var valor2 = myapppersistencia.getInt(claveEntero, 0).toString().toInt()
+            binding.guardarEntero.setText(getString(R.string.valor_entero, valor2))
 
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
+            var texto = binding.ingTexto.text.toString()
+            myapppersistencia.edit().putString(textoingresado, texto).apply()
+            var texto2 = myapppersistencia.getString(textoingresado, "").toString()
+            binding.guardarTexto.setText(getString(R.string.texto_almacenado_1_s, texto2))
 
 
+            var decimal2 = binding.editDecimal.text.toString().toFloat()
+            myapppersistencia.edit().putFloat(decimal, decimal2).apply()
+            var valor3 = myapppersistencia.getFloat(decimal, 0.0f).toString().toFloat()
+            binding.guardarDecimal.setText(getString(R.string.valor_decimal, valor3.toString()))
 
-            view.findViewById<Button>(R.id.textview_first).setOnClickListener {
-                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-            }
+
+        }
+        binding.buttonBorrar.setOnClickListener {
+            myapppersistencia.edit().remove(claveEntero).apply()
+            myapppersistencia.edit().remove(decimal).apply()
+            myapppersistencia.edit().remove(textoingresado).apply()
+            binding.editNumber.setText("")
+            binding.guardarEntero.setText("")
+            binding.ingTexto.setText("")
+            binding.guardarTexto.setText("")
+            binding.editDecimal.setText("")
+            binding.guardarDecimal.setText("")
         }
 
-        fun Save() {
 
-            val mUserKey = ""
-            var mUserRead = myapppersistencia.getString(mUserKey, "")
-            if (mUserRead != null) {
-                Log.d("TAG", mUserRead)
-            }
-
-
-            //obtener un int
-            myapppersistencia.getInt("TestInt", -1)
-
-
-            //obtener un boolean
-            myapppersistencia.getBoolean("TestBoolean", false)
-
-
-            //obtener un String
-            myapppersistencia.getString("TestString", "NotFound")
-
-            //obtener un Set<String>
-            myapppersistencia.getStringSet("TestStringSet", setOf())
-        }
     }
 
 }
-
 
 
